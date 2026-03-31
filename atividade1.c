33167d2841cad1b315f414373c9db5b496afc956
Melquizedequedos <melquizedequesantos13@gmail.com>
2026-03-31 20:49:32 -0300

    atividade 1 concluida

--- atividade1.c
+++ atividade1.c
@@ -1,1 +1,45 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+           //entrada de dados 
+char categoria;
+          // declaraçao de variaveis 
+
+printf("Digite uma a letra da categoria para encontramos o veiculo que e permitido dirigir.\n");
+printf("Digite apenas a letra corespondente a categoria.\n");
+    scanf("%c",&categoria);
+categoria = toupper(categoria); // converte para maiúscula
+switch (categoria)    
+        // processamento
+{
+
+    case 'A':
+              printf("Moto.\n");
+              break;
+    case 'B':
+              printf("Carro.\n");
+              break;
+    case 'C':
+              printf("Caminhão.\n");
+              break;
+    case 'D':
+              printf("Ônibus.\n");
+              break;
+    case 'E':
+              printf("Carreta.\n");
+              break;
+         //saidas
+              
+    default:
+             printf("Essa letra nao coresponde a nenhuma categoria!\n");
+             printf("Você pode nao pode dirigir!!!");
+    
+}
+
+
+
+
+
+    return 0;
+}
