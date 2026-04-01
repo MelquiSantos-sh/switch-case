31f2ea30208847f615ceba4a0dd2f7193982b745
Melquizedequedos <melquizedequesantos13@gmail.com>
2026-03-31 21:19:02 -0300

    ajuste atividade 2

--- atividade2.c
+++ atividade2.c
@@ -3,17 +3,40 @@
 int main(){
           //declaração de variaveis
 
-    Int dia;
-    
+    int dia;
+    printf("Digite um numero de 1 a 7 corespondente ao dia da semana.\n");
+    scanf("%d",&dia);
+         // processamento 
+    switch (dia)
+    { 
+
+        case 1:
+                    printf("Domingo\n");
+                    break;
+        case 2:
+                    printf("Segunda\n");
+                    break;
+        case 3:
+                    printf("Terça-Feira\n");
+                    break;
+        case 4:
+                    printf("Quarta-Feira\n");
+                    break;
+        case 5:
+                    printf("Qunita-Feira\n");
+                    break;
+        case 6:
+                    printf("Sexta-feira\n");
+                    break;
+        case 7:
+                    printf("Sabado\n");
+                    break;
 
 
+        default:    
+                    printf("Esse numero nao coresponde a nenhum dia da semana.\n");
 
-
-
-
-
-
-
+    }
 
 
     return 0;
